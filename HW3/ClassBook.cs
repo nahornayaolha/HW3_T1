a29f93d7ca9341c17f8663926fc74113201451e8
Olha Nahorna <olha_nahorna@epam.com>
2019-03-13 23:38:14 +0200

    hw3_t1

---
+++ HW3/ClassBook.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW3
+{
+    class ClassBook
+    {
+        string surname;
+        int mark;
+        private Dictionary<string, int> classbook = new Dictionary<string, int>();
+
+        public void AddToClassBook(string added)
+        {
+            string[] surname_mark = added.Split(':');
+            surname = surname_mark[0];
+            mark = Convert.ToInt32(surname_mark[1]);
+            if (mark < 1 || mark > 5)
+            {
+                Console.WriteLine("Mark should be a number from 1 to 5. Try again");
+            }
+            classbook.Add(surname, mark);
+
+        }
+
+        public void listOfPupils()
+        {
+            foreach (KeyValuePair<string, int> keyValue in classbook)
+            {
+                Console.WriteLine(keyValue.Key + ":" + keyValue.Value);
+            }
+        }
+
+        public bool isNumber(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= '0' && str[i] <= '9')
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public void findBook()
+        {
+
+            foreach (KeyValuePair<string, int> keyValue in classbook)
+            {
+                Console.WriteLine("Please enter surname to see student`s mark or enter 'exit' to exit");
+                
+                surname = Console.ReadLine();
+
+                if (classbook.TryGetValue(surname, out mark))
+                {
+                    Console.WriteLine("Student " + surname + " received a " + mark);
+                }
+                else if (surname == "exit")
+                {
+                    break;
+                }
+                else if (!classbook.ContainsKey(surname))
+                {
+                    Console.WriteLine("Input has wrong format");
+                }
+                
+                
+             }
+        }
+
+    }
+}
+
+
+
--- HW3/Program.cs
+++ HW3/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HW3
 {
@@ -10,6 +6,39 @@ namespace HW3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter students and their marks: (enter 'stop' to finish):");
+            ClassBook list = new ClassBook();
+
+            string added;
+            bool isInputStoped = true;
+            
+            while (isInputStoped)
+            {
+                try
+                {
+                    added = Console.ReadLine();
+
+                    if (added == "stop")
+                    {
+                        isInputStoped = false;
+                        break;
+                    }
+                   else
+                    {
+                        list.AddToClassBook(added);
+                    }
+                }
+                catch (System.IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Input has wrong format! Desired format: 'Surname:Mark'");
+                    Console.WriteLine("Mark should be a number from 1 to 5'");
+                    continue;
+                }
+            }
+            list.listOfPupils();
+            list.findBook();
+                            
+            Console.ReadKey();
         }
     }
 }
